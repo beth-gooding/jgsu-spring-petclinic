d7bc7f0c43b50c99ca7bb53219245e6c2aa658f9
Beth Gooding <78368143+beth-gooding@users.noreply.github.com>
2021-12-31 09:49:42 +0000

    Fix test again
    To trigger the same Jenkins actions as the last commit

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("bob");
+		person.setFirstName("");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
